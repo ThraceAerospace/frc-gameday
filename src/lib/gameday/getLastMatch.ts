2629fa7161f7688b0a142083f45df316aa6a21b4
= <=>
2026-09-21 00:58:52 -0400

    general cleanup

--- src/lib/gameday/getLastMatch.ts
+++
@@ -1,7 +0,0 @@
-export function getLastMatch(matches: any[]) {
-  return (
-    matches
-      .filter((m) => m.actual_time != null)
-      .sort((a, b) => (b.actual_time ?? 0) - (a.actual_time ?? 0))[0] ?? null
-  );
-}
